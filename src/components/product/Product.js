6d4b0ea8feb5e82f552f3996c0ff3b7b0c1df484
mykolaturchyn <tyrchnmikigt@gmail.com>
2022-01-06 10:42:10 +0200

    init

---
+++ src/components/product/Product.js
@@ -0,0 +1,9 @@
+export default function Product({title,price}) {
+    return (
+        <div>
+            <h2>{title}</h2>
+            <h3>price {price}</h3>
+            <hr/>
+        </div>
+    );
+}
