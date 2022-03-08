37bb1c3e42a795c4288d111b95d334902967371c
Микола Турчин <tyrchnmikigt@gmail.com>
2022-03-08 20:29:13 +0200

    todo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-export default function App() {
-    return (
-        <div>
-            App
+import {useEffect, useReducer, useState} from "react";
 
+export default function App() {
+    let reducer = (actin, payload) => {
+
+    }
+    const [counter, setCounter] = useState(1);
+useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/todos${counter}`).then(value => setCounter(value))
+})
+    // const fetch = async () => {
+    //     const response = await fetch(`https://jsonplaceholder.typicode.com/todos${counter}`)
+    // }
+
+    const [state, dispatch] = useReducer(reducer, null)
+    return (
+
+
+        <div>
+            {counter}<br/>
+            <button onClick={() => setCounter(prevState => prevState + 1)}>inc</button>
         </div>
     );
 }
